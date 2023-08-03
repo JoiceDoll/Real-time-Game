0e832a48b93c1447c23359d36dda781f3c8aff5a
Joice Doll <joice.doll2@yahoo.com>
2023-08-02 23:12:00 -0300

    game over background

--- src/pages/gamePage.js
+++ src/pages/gamePage.js
@@ -1,6 +1,7 @@
+const marioBros = document.querySelector(".mario");
+
 function renderEvent(controlEvent) {
   if (controlEvent) {
-    const marioBros = document.querySelector(".mario");
     if (marioBros) {
       marioBros.style.bottom = "60%";
       function downMarioTimeOut() {
@@ -12,3 +13,23 @@ function renderEvent(controlEvent) {
     }
   }
 }
+const pipe = document.querySelector(".pipeImage");
+const background = document.querySelector(".game-over-background")
+const loopPiper = setInterval(() => {
+  const offSetPositionPipe = pipe.offsetLeft;
+  const offSetTopMario = marioBros.offsetTop;
+
+  if (
+    offSetPositionPipe <= 150 &&
+    offSetPositionPipe > 0 &&
+    offSetTopMario === 453
+  ) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${offSetPositionPipe}px`;
+    marioBros.setAttribute("src", "../images/game-over.png");
+    marioBros.style.width = "50px";
+    marioBros.style.height = "85px";
+    background.style.display = "block"
+
+  }
+}, 10);
